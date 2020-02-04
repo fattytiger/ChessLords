9f6e63c14d0ee56ca3609131996c168fc6814e8b
panghu <315782792@qq.com>
2020-02-04 18:48:47 +0800

    Add logindata file

---
+++ client-side/assets/script/network/LoginData.js
@@ -0,0 +1,37 @@
+
+
+const LoginData =  cc.Class({
+    properties: {
+
+    },
+    ctor:function(){
+
+        let account_id = arguments[0]
+        let account_name = arguments[1]
+        let blockchain = arguments[2]
+        let network    = arguments[3]
+
+        this.hero_id = account_id
+        this.hero_name = account_name
+        this.blockchain = blockchain
+        this.network  = network
+    },
+
+    getHeroID:function(){
+        return this.hero_id
+    },
+
+    getHeroName:function(){
+        return this.hero_name
+    },
+
+    getNetType:function(){
+        return this.network
+    },
+
+    getBlockchainType:function(){
+        return this.blockchain
+    }
+
+
+});
