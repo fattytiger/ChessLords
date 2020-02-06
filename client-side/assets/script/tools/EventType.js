d8dcbb63bd411e676278927f5ee404f858c36f59
panghu <315782792@qq.com>
2020-02-06 22:31:20 +0800

    Add pop up handle event

--- client-side/assets/script/tools/EventType.js
+++ client-side/assets/script/tools/EventType.js
@@ -1,8 +1,10 @@
 var EventType = {}
 
 const LOAD_ISOVER = "LOAD_ISOVER"
+const POP_UP = "POP_UP"
 
 module.exports = {
     EventType:EventType,
-    LOAD_ISOVER:LOAD_ISOVER
+    LOAD_ISOVER:LOAD_ISOVER,
+    POP_UP:POP_UP
 }
