40b2231f7ba358eedeadc171e0f4817b38135b82
panghu <315782792@qq.com>
2020-02-06 22:35:16 +0800

    Format the Net file

--- client-side/assets/script/network/Net.js
+++ client-side/assets/script/network/Net.js
@@ -3,7 +3,7 @@ const EventType = require('EventType');
 cc.Class({
     extends: cc.Component,
 
-    ctor: function() {
+    ctor: function () {
         this.ip = '';
         this.socket = null;
         this.heartTime = 10;
@@ -11,30 +11,30 @@ cc.Class({
         this.maxConnectTimes = 5;
         this.handlers = {};
         this.constants = {
-            HERO_SET_TROOPS_PROPORTIONS:            4015,
-            MAP_DATA:                               6001,
-            MAP_ACCOUNT:                            8007,
-            GENERAL_ERROR:                          9001,
-            MOVEMENT_NOTIFICATION:                  10011,
-            HERO_APPEARANCE_CHANGE_NOTIFICATION:    10016,
-            BUILDING_REWARD:                        10018,
-            ARMORY_ADD_ITEMS:                       12001,
-            ARMORY_REMOVE_ITEMS:                    12002,
-            ARMORY_GET_DATA:                        12003,
-            ARMORY_SET_MILITARY_ADVANCEMENT:        12005,
-            REFILL_HERO_TROOPS:                     13001,
-            MANAGE_BUILDING_DEFENSE:                13003,
-            REGISTER_USER:                          14001,
-            LOGIN_USER:                             14002,
-            IS_USER_LOGGED_IN:                      14003,
-            LOGOUT_USER:                            14004,
-            FIRST_JOIN_MAP:                         10019,
-            GET_TASK_LIST:                          15001,
-            CLAIM_TASK_REWARD:                      15002,
+            HERO_SET_TROOPS_PROPORTIONS: 4015,
+            MAP_DATA: 6001,
+            MAP_ACCOUNT: 8007,
+            GENERAL_ERROR: 9001,
+            MOVEMENT_NOTIFICATION: 10011,
+            HERO_APPEARANCE_CHANGE_NOTIFICATION: 10016,
+            BUILDING_REWARD: 10018,
+            ARMORY_ADD_ITEMS: 12001,
+            ARMORY_REMOVE_ITEMS: 12002,
+            ARMORY_GET_DATA: 12003,
+            ARMORY_SET_MILITARY_ADVANCEMENT: 12005,
+            REFILL_HERO_TROOPS: 13001,
+            MANAGE_BUILDING_DEFENSE: 13003,
+            REGISTER_USER: 14001,
+            LOGIN_USER: 14002,
+            IS_USER_LOGGED_IN: 14003,
+            LOGOUT_USER: 14004,
+            FIRST_JOIN_MAP: 10019,
+            GET_TASK_LIST: 15001,
+            CLAIM_TASK_REWARD: 15002,
         };
     },
 
-    addHandler: function(evtName, fn, canRegMore) {
+    addHandler: function (evtName, fn, canRegMore) {
         evtName = evtName.toString();
         if (this.handlers[evtName] && !canRegMore) {
             cc.error('evt: [' + evtName + '] handler has been registered already.');
@@ -46,20 +46,20 @@ cc.Class({
         }
     },
 
-    removeHandler: function(evtName) {
+    removeHandler: function (evtName) {
         evtName = evtName.toString();
         cc.zz.fire.un(evtName);
         this.handlers[evtName] = null;
     },
 
-    removeAllHandler: function() {
+    removeAllHandler: function () {
         for (var evtName in this.handlers) {
             cc.zz.fire.un(evtName);
         }
         this.handlers = {};
     },
 
-    addHandlers: function(fns) {
+    addHandlers: function (fns) {
         for (var evtName in fns) {
             this.addHandler(evtName, fns[evtName]);
         }
@@ -72,7 +72,7 @@ cc.Class({
         }
     },
 
-    connect: function(ip, fnConnect, fnError) {
+    connect: function (ip, fnConnect, fnError) {
         this.ip = ip;
         this.fnConnect = fnConnect;
         this.fnError = fnError;
@@ -85,7 +85,7 @@ cc.Class({
         }
     },
 
-    _onopen: function() {
+    _onopen: function () {
         if (this.connectTimes) {
             cc.zz.fire.fire(EventType.POP_UP, cc.zz.Popup.TYPE.RECONNECTED_TO_SERVER.id, {});
         }
@@ -97,7 +97,7 @@ cc.Class({
         }
     },
 
-    _onclose: function(err) {
+    _onclose: function (err) {
         if (this.connectTimes < this.maxConnectTimes) {
             this.scheduleOnce(function () {
                 this.socket = new WebSocket(this.ip);
@@ -115,7 +115,7 @@ cc.Class({
                     }
                 }
             }, 3)
-            cc.error("Server connection has been disconnected: " + this.connectTimes + "");
+            cc.zz.fire.fire(EventType.POP_UP, cc.zz.Popup.TYPE.FACING_SERVER_CONNECTION_ISSUE.id, {});
         } else {
             if (this.fnError) {
                 this.fnError(-1);
@@ -124,38 +124,39 @@ cc.Class({
             else {
                 cc.zz.fire.fire(EventType.POP_UP, cc.zz.Popup.TYPE.NO_SERVER_CONNECTION.id, {});
             }
+            cc.zz.fire.fire(EventType.POP_UP, cc.zz.Popup.TYPE.NO_SERVER_CONNECTION.id, {});
             cc.error("Server attempted to reconnect to server, but failed: " + this.connectTimes + "");
         }
     },
 
-    _onmessage: function(msg) {
-       
+    _onmessage: function (msg) {
+
+        
         var data = JSON.parse(msg.data);
         // if (data[0] == 10007) {
-            // console.log("Went to hero Profile");
-            // this.unschedule(this.sendHeart);
-            // this.socket.close();
+        // console.log("Went to hero Profile");
+        // this.unschedule(this.sendHeart);
+        // this.socket.close();
         // }
         if (data != null && data != undefined) {
             var msgID = parseInt(data[0]);
             var timeStamp = data[1];
             var msgData = data[2];
-
+            console.log(msgID)
             cc.zz.utils.realTime = timeStamp;
             if (msgID === 9001) {
-                cc.error('Error: ' , data);
+                cc.error('Error: ', data);
                 cc.zz.fire.fire(msgID.toString(), msgData);
             } else if (msgID === 8003) {
                 // cc.log('hearbeat');
             } else {
-                console.log(data);
                 cc.zz.fire.fire(msgID.toString(), msgData);
             }
             this.resetCheckHeart();
         }
     },
-    send: function (...args) {   
-        cc.log(args);     
+    send: function (...args) {
+        cc.log(args);
         this.socket.send(JSON.stringify(args));
     },
 
@@ -169,20 +170,20 @@ cc.Class({
     },
 
     sendHeart: function () {
-        this.socket.send(JSON.stringify([8003,[]]));
+        this.socket.send(JSON.stringify([8003, []]));
     },
 
     // TODO: Move to Internet Utility 
-    getIpAddress:function(blockchain, network) {
-        if(blockchain === 'cocosbcx'){
-            if(network === 'privatenet'){
+    getIpAddress: function (blockchain, network) {
+        if (blockchain === 'cocosbcx') {
+            if (network === 'privatenet') {
                 return "ws://127.0.0.1:8000"
             }
         }
 
-        if(blockchain == 'neo') {
+        if (blockchain == 'neo') {
             if (network == 'mainnet') {
-                return "wss://neo.blocklords.io:443/backend/";  
+                return "wss://neo.blocklords.io:443/backend/";
             }
             else if (network == 'testnet') {
                 return "ws://153.92.5.151:8085/";
@@ -191,22 +192,22 @@ cc.Class({
                 return "ws://localhost:8000/";
             }
         }
-        else if(blockchain == 'tron') {
+        else if (blockchain == 'tron') {
             if (network == 'privatenet') {
                 return "ws://127.0.0.1:8000/";//"ws://153.92.5.151:8086/";
             }
-            else if ( cc.zz.LoginData.getNetType() == 'testnet' ) {
+            else if (cc.zz.LoginData.getNetType() == 'testnet') {
                 return "ws://153.92.5.151:8083/";
             }
             else {
                 return "wss://tron.blocklords.io:443/backend/"; // "ws://127.0.0.1:8000/"
             }
-        } 
-        else if(blockchain == 'ethereum') {
+        }
+        else if (blockchain == 'ethereum') {
             if (network == 'privatenet') {
                 return "ws://127.0.0.1:8000/";//"ws://153.92.5.151:8086/";
             }
-            else if (network == 'testnet' ) {
+            else if (network == 'testnet') {
                 return "ws://153.92.5.151:8088/";
             }
             else {
@@ -214,7 +215,7 @@ cc.Class({
             }
         }
 
-        cc.error ('Failed to find Server host of '+blockchain+' '+network+' server!');
+        cc.error('Failed to find Server host of ' + blockchain + ' ' + network + ' server!');
         return 'ws://localhost:8888';
     },
 });
