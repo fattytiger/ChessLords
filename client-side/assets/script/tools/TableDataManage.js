9b423c4b8b9f360328a2ed26e499a29324357dce
panghu <315782792@qq.com>
2020-02-09 00:05:11 +0800

    Add TableDataManager.js at client side

--- client-side/assets/script/tools/TableDataManage.js
+++ client-side/assets/script/tools/TableDataManage.js
@@ -1,8 +1,8 @@
 cc.Class({
     ctor:function(){
         this.jsonArr = {}
-        // var BlocksData = require('blocks-info-data')
-        // this.BlocksData = new BlocksData()
+        let BlocksData = require('blocks-info-data')
+        this.BlocksData = new BlocksData()
 
         let Language = require('Language');
         this.Language = new Language()
@@ -11,7 +11,7 @@ cc.Class({
         for(var key in table){
             this.jsonArr[table[key].name] = table[key].json
         }
-        // this.BlocksData.initData(this.jsonArr.block_blocks_info)
+        this.BlocksData.initData(this.jsonArr.blocks_info)
         this.Language.initData(this.jsonArr.language_first_name,this.jsonArr.language_last_name,this.jsonArr.language_item_name,this.jsonArr.language_item_des,this.jsonArr.language_neo_ui); this.Language.initData(this.jsonArr.language_first_name,this.jsonArr.language_last_name,this.jsonArr.language_item_name,this.jsonArr.language_item_des,this.jsonArr.language_neo_ui);
     },
 })
