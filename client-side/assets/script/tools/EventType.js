e210c891a5b87e46ee7f60e4b183bf18c587b69e
panghu <315782792@qq.com>
2020-02-09 00:05:49 +0800

    Add LOAD_ATLAS_RESOURCE command event at client side

--- client-side/assets/script/tools/EventType.js
+++ client-side/assets/script/tools/EventType.js
@@ -1,10 +1,12 @@
 var EventType = {}
 
 const LOAD_ISOVER = "LOAD_ISOVER"
-const POP_UP = "POP_UP"
+const POP_UP = "POP_UP"                         
+const LOAD_ATLAS_RESOURCE = "LOAD_ATLAS_RESOURCE"           //bind with in sprite-manger.js
 
 module.exports = {
     EventType:EventType,
     LOAD_ISOVER:LOAD_ISOVER,
-    POP_UP:POP_UP
+    POP_UP:POP_UP,
+    LOAD_ATLAS_RESOURCE:LOAD_ATLAS_RESOURCE
 }
