1db48a097e1a3fcb1cfa9a83cf3bffa8a9e4c50f
panghu <315782792@qq.com>
2020-02-27 13:24:47 +0800

    Server side
    Perfect the handler module

--- server-site/chess-lord-server/handler/index.js
+++ server-site/chess-lord-server/handler/index.js
@@ -1,37 +1,39 @@
 const COMMANDS = require('../commands')
-const LOGIN_SERVER = require('../hero_part/login_server')
-module.exports = handle = {
-    
-    receiveRequest:function(client){
+const LOGIN_SERVER = require('../heropart/LoginServer')
+const MAP = require('../map_part/map')
+
+module.exports = {
+    onClientMessage: function (client) {
         //listen the message
-        client.on("message",function message(message){
+        client.on("message", function message(message) {
             //transfer the data from the client
             let transferData = JSON.parse(message)
             //the request code
-            let messageID    = transferData[0]
+            let messageID = transferData[0]
             //the request parameters
-            let parameters   = transferData[1]
+            let parameters = transferData[1]
 
             //when hero login the server
-            if(messageID === COMMANDS.HERO_LOGIN_SERVER){
-                //populate the parameter as client and parameters
-                LOGIN_SERVER.heroLoginServer(client,parameters)
+            if (messageID === COMMANDS.HERO_LOGIN) {
+                LOGIN_SERVER.login(client, parameters)
             }
-
             //when hero ready for game
-            if(messageID === COMMANDS.HERO_READ){
-                //illustrat the hero is ready for game
-                LOGIN_SERVER.heroReady(client,parameters)
+            if (messageID === COMMANDS.HERO_READY) {
+                
+                LOGIN_SERVER.ready(client, parameters)
+            }
+            //when player login in the game
+            if (messageID === COMMANDS.MAP_DATA) {
+                MAP.init_map(client, parameters)
             }
         })
-
-
+    },
+    onClientClose: function (client) {
         //listen the websoket close
-        client.on("close",function close(){
+        client.on("close", function close() {
             //illustrate the hero is closed of the game
             LOGIN_SERVER.heroCloseServer(client)
         })
     },
-
 }
 
