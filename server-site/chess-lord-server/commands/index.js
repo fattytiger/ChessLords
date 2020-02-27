11e266bbc4d2c8244f5d02d99fa5948f3b9d63f2
panghu <315782792@qq.com>
2020-02-27 13:27:55 +0800

    Server side
    Perfect the commands

--- server-site/chess-lord-server/commands/index.js
+++ server-site/chess-lord-server/commands/index.js
@@ -1,10 +1,15 @@
-
-
-
 module.exports = {
+    ERROR_COMMAND:4004,
 
-    HERO_LOGIN_SERVER:  8005,
-    HERO_READ:  8006,
-    FIND_ANEMY: 9000
+    HERO_LOGIN:  8005,
+    HERO_READY:  8006,
+    INTO_GAME:   8007,
 
+    FIND_ANEMY: 9000,
+    MAP_DATA:   6001,
+
+
+    TIP_MESSAGE:{
+        "WAITTING_PLAYER":'please watting another one'
+    }
 }
