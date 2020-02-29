f15d216542ab878ffd4fbb540e99e7005fb16455
panghu <315782792@qq.com>
2020-02-29 22:59:11 +0800

    Server side
    Perfect the mapdata function in map.js

--- server-site/chess-lord-server/mappart/map.js
+++ server-site/chess-lord-server/mappart/map.js
@@ -10,47 +10,101 @@ module.exports = {
     mapdata:function(client,parameters){
         //get hero id
         let heroID = parameters[0]
-        console.log(heroID)
 
         //find the hero camp
         heroOptions.findHeroByHeroID(heroID)
         .then((document) => {
-            return document.camp
+            let troops = document.troops
+            let heroCamp = document.camp
+            if(troops.length === 0){
+                this.originMapData(heroID,heroCamp)
+            }
+            if(troops.length !== 0){
+                this.masterMapData(heroID,heroCamp)
+            }
         })
+    },
+
+    originMapData:function(hero_id){
+        let heroID = hero_id
         //create the solider
-        .then(camp => {
-            if(camp === RED_CAMP){
+        heroOptions.findHeroByHeroID(heroID)
+        .then(document => {
+            this.camp = document.camp
+            if(this.camp === RED_CAMP){
                 return troopOptions.createRedSoldierByHeroID(heroID)
             }
-            if(camp === BLUE_CAMP){
+            if(this.camp === BLUE_CAMP){
                 return troopOptions.createBlueSoldierByHeroID(heroID)
             }
-        }).then((document) => {
+        }).then(document => {
             if(document.camp === RED_CAMP){
+                return heroOptions.pushTroopByHeroID(document.hero_id,document._id)
+            }
+            if(document.camp === BLUE_CAMP){
+                return heroOptions.pushTroopByHeroID(document.hero_id,document._id)
+            }
+        }).then(res => {
+            if(this.camp === RED_CAMP){
                 return troopOptions.createRedCavalryByHeroID(heroID)
             }
-            if(document.camp === BLUE_CAMP){
+            if(this.camp === BLUE_CAMP){
                 return troopOptions.createBlueCavalryByHeroID(heroID)
             }
-        }).then((document) => {
-            if(document.camp === RED_CAMP){
+        }).then(document => {
+            if(this.camp === RED_CAMP){
+                return heroOptions.pushTroopByHeroID(document.hero_id,document._id)
+            }
+            if(this.camp === BLUE_CAMP){
+                return heroOptions.pushTroopByHeroID(document.hero_id,document._id)
+            }
+        }).then(res => {
+            if(this.camp === RED_CAMP){
                 return troopOptions.createRedArcherByHeroID(heroID)
             }
-            if(document.camp === BLUE_CAMP){
+            if(this.camp === BLUE_CAMP){
                 return troopOptions.createBlueArcherByHeroID(heroID)
             }
-        }).then((document) => {
-            console.log(document)
+        }).then(document => {
+            if(this.camp === RED_CAMP){
+                return heroOptions.pushTroopByHeroID(document.hero_id,document._id)
+            }
+            if(this.camp === BLUE_CAMP){
+                return heroOptions.pushTroopByHeroID(document.hero_id,document._id)
+            }
+        }).then(res => {
+            return heroOptions.findHeroByHeroID(heroID)
+        }).then(document => {
+            this.sendTroops(heroID,document.troops)
         })
+    },
 
-        //crate the cavalry
+    sendTroops:function(hero_id,troops){
+        let heroID = hero_id
+        let troopArr = []
+        troops.forEach(troop => {
+            troopOptions.findTroopByObjecyID(troop)
+            .then(document => {
+                //push the troops
+                troopArr.push(document)
+                if(troopArr.length === 3){
+                    console.log('send data to client')
+                    let hero = {}
+                    hero.hero_id = heroID
+                    hero.troops = troopArr
+                    clientManager.sendBroadCastData(COMMANDS.MAP_DATA,hero)
+                }
+            })
+        });
+    },
 
-        //create the archer
 
-
-        //save the troop information interms of the camp
-
-        //give the troop correct position
+    masterMapData:function(hero_id,hero_camp){
+        let heroID = hero_id
+        heroOptions.findHeroByHeroID(heroID)
+        .then(document => {
+            this.sendTroops(heroID,document.troops)
+        })
     }
     
 }
