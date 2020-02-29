acfe56a33b9b3d66627781d7ffb5f5d9abe07c3a
panghu <315782792@qq.com>
2020-02-29 22:52:17 +0800

    Client side
    Delete the useless coding in block.js

--- client-side/assets/script/map/block.js
+++ client-side/assets/script/map/block.js
@@ -102,7 +102,6 @@ cc.Class({
     },
     mouseRightClick() {
         cc.log(this.id,this.move)
-        // cc.zz.net.send(6015, [this.id, 1])
     },
 
     initOriginData(table_info) {
@@ -129,202 +128,14 @@ cc.Class({
         this.initBlockSprite()
         
     },
+
     initBlockSprite:function(){
         let atalasPath = `atalasElements/map`
         cc.zz.fire.fire(EventType.LOAD_ATLAS_RESOURCE,atalasPath,this.blockPic,(function(sprite){
             this.masterBlock.spriteFrame = sprite
         }).bind(this))
     },
-    
-    moveHighLight() {
-        this.rangeFlag = true
-        this.canMove   = true
-        if(this.move === "0"){
-            return
-        }
-        
-        //hilight the movement block
-        cc.zz.fire.fire(EventType.CHANGE_BLOCK_LIGHT, this.blockPic, this.movementBlock)
 
-        //flag the edge of the block
-        cc.zz.fire.fire(EventType.SHOW_MOVE_BLOCK_HIGHLIGHT,this.id)
-
-        //set the node as active
-        this.movementBlock.node.active = true
-
-        //show the movement block animation
-        this.movementBlock.node.runAction(cc.fadeIn(1))
-        
-    },
-
-    leaveHighLight() {
-        this.rangeFlag = false
-        this.canMove   = false
-
-        if(this.move === "0"){
-            return
-        }
-
-        //reback the movement block
-        cc.zz.fire.fire(EventType.CHANGE_BLOCK_SPRITE, this.blockPic, this.movementBlock)
-
-        //reback the edge of the block
-        cc.zz.fire.fire(EventType.HIDE_MOVE_BLOCK_HIGHLIGHT,this.id)
-
-        //hide the movement block animation
-        this.movementBlock.node.runAction(cc.fadeOut(1))
-        
-    },
-
-    getRangeFlag:function(){
-        return this.rangeFlag
-    },
-
-    setMoveDirectionLight:function(direction){
-        if(direction === "UP"){
-            this.rangeHighlight[this.HIGHLIGHT_DIRECTION.UP].node.active = true
-        }
-
-        if(direction === "DOWN"){
-            this.rangeHighlight[this.HIGHLIGHT_DIRECTION.DOWN].node.active = true
-        }
-
-        if(direction === "RIGHT"){
-            this.rangeHighlight[this.HIGHLIGHT_DIRECTION.RIGHT].node.active = true
-        }
-
-        if(direction === "LEFT"){
-            this.rangeHighlight[this.HIGHLIGHT_DIRECTION.LEFT].node.active = true
-        }
-    },
-
-
-
-
-    setDirectionHighlight:function(wallet_address,direction){
-        let selfWallet = cc.zz.LoginData.getWallet()
-
-        //set self direction highlight
-        if(wallet_address === selfWallet){
-
-            if(direction === "UP"){
-                this.selfHighLight[this.HIGHLIGHT_DIRECTION.UP].node.active = true
-            }
-
-            if(direction === "DOWN"){
-                this.selfHighLight[this.HIGHLIGHT_DIRECTION.DOWN].node.active = true
-            }
-
-            if(direction === "RIGHT"){
-                this.selfHighLight[this.HIGHLIGHT_DIRECTION.RIGHT].node.active = true
-            }
-
-            if(direction === "LEFT"){
-                this.selfHighLight[this.HIGHLIGHT_DIRECTION.LEFT].node.active = true
-            }
-        }
-
-        if(wallet_address !== selfWallet){
-            if(direction === "UP"){
-                this.otherHighLight[this.HIGHLIGHT_DIRECTION.UP].node.active = true
-            }
-
-            if(direction === "DOWN"){
-                this.otherHighLight[this.HIGHLIGHT_DIRECTION.DOWN].node.active = true
-            }
-
-            if(direction === "RIGHT"){
-                this.otherHighLight[this.HIGHLIGHT_DIRECTION.RIGHT].node.active = true
-            }
-
-            if(direction === "LEFT"){
-                this.otherHighLight[this.HIGHLIGHT_DIRECTION.LEFT].node.active = true
-            }
-        }
-    },
-
-    conqueredHighLight: function (wallet_address) {
-
-        let walletAddress = wallet_address
-        let selfWallet = cc.zz.LoginData.getWallet()
-
-        
-
-        if (walletAddress == null) {
-            console.warn(`please give the wallet parameter`);
-            return 
-        }
-
-        //flag the owner of this block
-        this.conqueredWallet = walletAddress
-
-        //set as your occupied block
-        if (walletAddress === selfWallet) {
-            cc.zz.fire.fire(EventType.CHANGE_BLOCK_LIGHT, this.blockPic, this.masterBlock)
-        }
-        //set as other occupied block
-        if (walletAddress !== selfWallet) {
-            cc.zz.fire.fire(EventType.CHANGE_BLOCK_LIGHT, this.blockPic, this.masterBlock)
-        }
-
-        //set build block highlight
-        cc.zz.fire.fire(EventType.SHOW_BUILD_BLOCK_HIGHLIGHT,walletAddress,this.id)
-    },
-
-    
-
-    getConquerInfo:function(){
-        return this.conqueredWallet
-    },
-
-    onHighlightSelfHeroBlock:function(){
-        for (let index = 0; index < this.selfHighLight.length; index++) {
-            this.selfHighLight[index].node.active =  true
-        }
-    },
-    unHighlightSelfHeroBlock:function(){
-        for (let index = 0; index < this.selfHighLight.length; index++) {
-            this.selfHighLight[index].node.active =  false
-        }
-    },
-    onHighlightOtherHeroBlock:function(){
-        for (let index = 0; index < this.otherHighLight.length; index++) {
-            this.otherHighLight[index].node.active = true
-        }
-    },
-
-    unHighlightOtherHeroBlock:function(){
-        for (let index = 0; index < this.otherHighLight.length; index++) {
-            this.otherHighLight[index].node.active = false
-        }
-    },
-    onHighLightMoveBlock:function(){
-        for (let index = 0; index < this.rangeHighlight.length; index++) {
-            this.rangeHighlight[index].node.active = true
-        }
-    },
-    unHighLightMoveBlock:function(){
-        for (let index = 0; index < this.rangeHighlight.length; index++) {
-            this.rangeHighlight[index].node.active = false
-        }
-    },
-
-    reverseBuildingArround:function(){
-        this.buildingAround = false
-    },
-    setBuildingArround:function(){
-        this.buildingAround = true
-    },
-    getBuildingArround:function(){
-        return this.buildingAround
-    },
-
-    setStandHero:function(){
-        this._standHero = !this._standHero
-    },
-    getStandHero:function(){
-        return this._standHero
-    },
 
 
 });
