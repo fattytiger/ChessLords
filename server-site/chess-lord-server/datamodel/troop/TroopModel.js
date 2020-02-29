39a98d4edb53926ff9cd8cb8bc72018c8edb3e12
tiger <315782792@qq.com>
2020-02-29 14:57:19 +0800

    Server side
    Add troop data model

---
+++ server-site/chess-lord-server/datamodel/troop/TroopModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const TroopSchema = new Schema({
+    hero_id:{type:String,required:true,max:100},
+    tile_from:{type:Number,required:true},
+    tile_to:{type:Number,required:true},
+    troop_type:{type:Number,required:true},
+    troop_name:{type:String,required:true},
+    troop_hp:{type:Number,required:true},
+    camp:{type:String,required:true}
+})
+
+
+module.exports = mongoose.model('troop',TroopSchema)
