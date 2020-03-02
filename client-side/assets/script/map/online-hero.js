126be434f0a1054eabf2dcb112f2019b601b5e4e
panghu <315782792@qq.com>
2020-03-02 13:48:23 +0800

    Client side
    Add initStaminaInterval function to recovery the stamina in online-hero.js

--- client-side/assets/script/map/online-hero.js
+++ client-side/assets/script/map/online-hero.js
@@ -1,4 +1,11 @@
 const EventType = require('EventType')
+
+const SOLDIER_CONSUME = [ 
+    { NAME:"soldier",CONSUME:20 ,RECOVERY_TIME: 10},
+    { NAME:"cavalry",CONSUME:10 ,RECOVERY_TIME: 10},
+    { NAME:"archer" ,CONSUME:25 ,RECOVERY_TIME: 5}
+ ]
+
 cc.Class({
     extends: cc.Component,
 
@@ -18,16 +25,26 @@ cc.Class({
         chooseFlag:{
             default:null,
             type:cc.Node
+        },
+        animDuration:{
+            default:0.5,
+            type:cc.Float
         }
     },
     ///////////////////////////////////////////////////////////////////////////////
     // Life cycle Methods
     ///////////////////////////////////////////////////////////////////////////////
+    ctor:function(){
+        this.MOVE_ACTION = {
+            MOVE_TO:"MOVE_TO",
+            ESCAPE_FROM:"ESCAPE_FROM"
+        }
+    },
+
     onLoad:function(){
         this.continueTime  = -1
         this.continueClick = false
         this.safeTime = 1000
-
         this.node.on(cc.Node.EventType.MOUSE_DOWN,this.mouseClick,this)
     },
 
@@ -63,6 +80,7 @@ cc.Class({
     ///////////////////////////////////////////////////////////////////////////////
     initConfig:function(){        
         this.blocksManager = cc.Canvas.instance.node.getComponent('blocks-manager')
+        this.moveLogic = this.node.getComponent('move-logic')
     },
     initOriginData:function(troop){
         this.initConfig()
@@ -70,14 +88,21 @@ cc.Class({
         this.troopID = troop._id
         this.tile_from = parseInt(troop.tile_from)
         this.tile_to   = parseInt(troop.tile_to)
+        this.havetimer = false
+        
+        this.moveProtect = false
 
         this.setHeroLocation()
         this.setTroopType(troop.troop_type)
         this.setTroopHP(troop.troop_hp)
+        this.setTroopStamina(troop.troop_stamina)
         this.setTroopMaster(troop.master_troop)
+        
     },
     setTroopType:function(troop_type){
-        this.troopType = parseInt(troop_type)
+
+        this.troop_type = parseInt(troop_type)
+        this.moveLogic.init(this.troop_type)
         let color = null
         let selfHeroID = cc.zz.LoginData.getHeroID()
         if(this.heroID === selfHeroID){
@@ -87,41 +112,178 @@ cc.Class({
             color = "c"
         }
         let path = "atalasElements/unit"
-        let url  = `unit_${color}0${this.troopType}`
+        let url  = `unit_${color}0${this.troop_type}`
+
         cc.zz.fire.fire(EventType.LOAD_ATLAS_RESOURCE,path,url,(function(sprite){
             this.heroSprite.spriteFrame = sprite
         }).bind(this))
     },
-    setTroopHP:function(troop_hp){
-        this.troopHP = parseInt(troop_hp)
+
+    getTroooType:function(){
+        return this.troop_type
     },
 
-    setTroopMaster:function(troop_master){
-        let selfHeroID = cc.zz.LoginData.getHeroID()
-        this.troopMaster = troop_master
-        if(this.troopMaster === true && selfHeroID === this.heroID){
-            cc.zz.fire.fire(EventType.CHOOSE_TROOP_FLAG,this.troopID)
-        }
+    onChooseFlag:function(){
+        this.chooseFlag.active = true
+        this.moveLogic.moveAction(this.MOVE_ACTION.MOVE_TO,this.tile_to)
     },
-    setHeroLocation:function(){
-        let pos = this.blocksManager.getBlockPositionByID(this.tile_from)
-        this.node.x = pos.x + 100
-        this.node.y = pos.y + 150
+    cancleChooseFlag:function(){
+        this.chooseFlag.active = false
+        this.moveLogic.moveAction(this.MOVE_ACTION.ESCAPE_FROM,this.tile_to)
+    },
+
+    setTroopTileFrom:function(tile_from){
+        this.tile_from = parseInt(tile_from)
+    },
+    setTroopTileTo:function(tile_to){
+        this.tile_to = parseInt(tile_to)
+    },
+    getTroopTileFrom:function(){
+        return this.tile_from
     },
 
     getTroopTileTO:function(){
         return this.tile_to
     },
-    onChooseFlag:function(){
-        this.master_troop = true
-        this.chooseFlag.active = true
+
+    setTroopHP:function(troop_hp){
+        this.troop_hp = parseInt(troop_hp)
+        this.troopHPPro.progress = this.troop_hp / 100
     },
-    cancleChooseFlag:function(){
-        this.master_troop = false
-        this.chooseFlag.active = false
+    setTroopStamina:function(troop_stamina){
+        this.troop_stamina =  parseInt(troop_stamina)
+        this.troopStamina.progress = this.troop_stamina / 100
     },
+    getTroopStamina:function(){
+        return this.troop_stamina
+    },
+
+    setTroopMaster:function(troop_master){
+        let selfHeroID = cc.zz.LoginData.getHeroID()
+        if(troop_master === true && selfHeroID === this.heroID){
+            cc.zz.fire.fire(EventType.CHOOSE_TROOP_FLAG,this.troopID)
+        }
+    },
+    setHeroLocation:function(){
+        let pos = this.blocksManager.getBlockPositionByID(this.tile_to)
+        this.node.x = pos.x + 100
+        this.node.y = pos.y + 150
+    },
+
+    
+
+    setTroopMoveProtect:function(){
+        this.moveProtect = !this.moveProtect
+    },  
+    getTroopMoveProtect:function(){
+        return this.moveProtect
+    },
+
+    
     ///////////////////////////////////////////////////////////////////////////////
     // Movement
     ///////////////////////////////////////////////////////////////////////////////
+    heroMove:function(tile_from,tile_to){
+        this.tile_from = parseInt(tile_from)
+        this.tile_to = parseInt(tile_to)
+        this.excuteMoveTask()  
+        this.initStaminaInterval()
+    },
+
+    initStaminaInterval:function(){
+
+        if(this.havetimer === true){
+            console.warn('there already have the interval');
+            return
+        }
+        
+        this.havetimer = true
+
+        let amount = SOLDIER_CONSUME[this.troop_type - 1].CONSUME
+        let interval = SOLDIER_CONSUME[this.troop_type - 1].RECOVERY_TIME
+        console.log(amount,interval)
+        this.schedule(() => {
+            let changedStamina = this.troop_stamina + amount
+            console.log(changedStamina)
+            if(changedStamina >= 100){
+                changedStamina = 100
+                this.setTroopStamina(changedStamina)
+            }
+            if(changedStamina < 100){
+                this.setTroopStamina(changedStamina)
+            }
+        },interval, cc.macro.REPEAT_FOREVER)
+    },
+
+
+    moveActionTo:function(tile_id){
+        this.moveLogic.moveAction(this.MOVE_ACTION.MOVE_TO,tile_id)
+    },
+
+    moveActionFrom:function(tile_from){
+        this.moveLogic.moveAction(this.MOVE_ACTION.ESCAPE_FROM,tile_from)
+    },
+
+    excuteMoveTask() {
+        //set hero protect when he is moving
+        this.setTroopMoveProtect()
+        //get the move path from the moveLogic.researchPath function
+        let movePath = this.moveLogic.researchPath(this.tile_from, this.tile_to)
+        //init the move array
+        this.moveTask = []
+        for (let index = 0; index < movePath.length; index++) {
+            let task = {}
+            if (movePath[index]) {
+                task.finish = false
+                task.to = movePath[index].id
+            }
+            if (movePath[index - 1]) {
+                task.from = movePath[index - 1].id
+            }
+            if (!movePath[index - 1]) {
+                task.from = this.tile_from
+            }
+            //set move task
+            this.moveTask.push(task)
+        }
+        this.moveOneBlock()
+    },
+
+    moveOneBlock: function () {
+        let selfHeroID = cc.zz.LoginData.getHeroID()
+        let task = {}
+        for (let index = 0; index < this.moveTask.length; index++) {
+            //find the next task 
+            if (this.moveTask[index].finish === false) {
+                task = this.moveTask[index]
+                break
+            }
+        }
+        //when the task is all excuted,unlock the hero protect 
+        if (task.finish == null) {
+            this.setTroopMoveProtect()
+            return
+        }
+        //hero run action
+        this.node.runAction(cc.sequence((cc.moveBy(this.animDuration, cc.v2(
+            -this.blocksManager.getBlockPositionByID(task.from).x + this.blocksManager.getBlockPositionByID(task.to).x,
+            -this.blocksManager.getBlockPositionByID(task.from).y + this.blocksManager.getBlockPositionByID(task.to).y
+        ))), cc.callFunc(function () {
+            if(selfHeroID === this.heroID){
+                this.moveActionFrom(task.from)
+                this.moveActionTo(task.to)
+            }
+
+            for (let index = 0; index < this.moveTask.length; index++) {
+                if (task.to == this.moveTask[index].to) {
+                    this.moveTask[index].finish = true
+                }
+            }
+            //when finished the action ,excute the next task right now
+            this.moveOneBlock()
+        }, this)))
+    },
+
     
+
 });
