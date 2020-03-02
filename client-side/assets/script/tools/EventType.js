eab4239a901681a448d7a7d0ddb309e246b750e5
panghu <315782792@qq.com>
2020-03-02 13:49:54 +0800

    Client side
    Add REQUEST_TROOP_MOVE global commands in EventType.js

--- client-side/assets/script/tools/EventType.js
+++ client-side/assets/script/tools/EventType.js
@@ -4,12 +4,14 @@ const LOAD_ISOVER = "LOAD_ISOVER"
 const POP_UP = "POP_UP"                         
 const LOAD_ATLAS_RESOURCE = "LOAD_ATLAS_RESOURCE"           //bind with in sprite-manger.js
 const CHOOSE_TROOP_FLAG = "CHOOSE_TROOP_FLAG"           //bind with in hero-manager.js
+const REQUEST_TROOP_MOVE = "REQUEST_TROOP_MOVE"           //bind with in hero-manager.js
 
 module.exports = {
     EventType:EventType,
     LOAD_ISOVER:LOAD_ISOVER,
     POP_UP:POP_UP,
     LOAD_ATLAS_RESOURCE:LOAD_ATLAS_RESOURCE,
-    CHOOSE_TROOP_FLAG:CHOOSE_TROOP_FLAG
+    CHOOSE_TROOP_FLAG:CHOOSE_TROOP_FLAG,
+    REQUEST_TROOP_MOVE:REQUEST_TROOP_MOVE
 
 }
