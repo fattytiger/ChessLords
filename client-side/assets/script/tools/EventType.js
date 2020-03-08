cb73168ce63332d8759c60baabe50f44e4f5c009
panghu <315782792@qq.com>
2020-03-08 14:48:54 +0800

    Client side
    Add REQUEST_FIGHT_OTHER and GET_ANIMATION_NAME common function

--- client-side/assets/script/tools/EventType.js
+++ client-side/assets/script/tools/EventType.js
@@ -8,6 +8,11 @@ const REQUEST_TROOP_MOVE = "REQUEST_TROOP_MOVE"           //bind with in hero-ma
 const SHOW_ATTACK_AREA = "SHOW_ATTACK_AREA"                 //bind with in hero-manager.js not use
 const HIDE_ATTACK_AREA = "HIDE_ATTACK_AREA"                 //bind with in hero-manager.js not use
 
+const REQUEST_FIGHT_OTHER = "REQUEST_FIGHT_OTHER"               //bind with in fight-manager.js
+
+const GET_ANIMATION_NAME = "GET_ANIMATION_NAME"             //bind with in animation-manager.js
+
+
 module.exports = {
     EventType:EventType,
     LOAD_ISOVER:LOAD_ISOVER,
@@ -16,6 +21,10 @@ module.exports = {
     CHOOSE_TROOP_FLAG:CHOOSE_TROOP_FLAG,
     REQUEST_TROOP_MOVE:REQUEST_TROOP_MOVE,
     SHOW_ATTACK_AREA:SHOW_ATTACK_AREA,
-    HIDE_ATTACK_AREA:HIDE_ATTACK_AREA
+    HIDE_ATTACK_AREA:HIDE_ATTACK_AREA,
+
+    REQUEST_FIGHT_OTHER:REQUEST_FIGHT_OTHER,
+    
+    GET_ANIMATION_NAME:GET_ANIMATION_NAME
 
 }
