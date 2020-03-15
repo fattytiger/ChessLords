e25955d1cff05a96c3d5a6dcaaba81f06cb81927
panghu <315782792@qq.com>
2020-03-15 20:42:01 +0800

    Server side
    Add BaseOptions.js to handler the base options with database

---
+++ server-site/chess-lord-server/databaseOption/BaseOptions.js
@@ -0,0 +1,66 @@
+const base = require('../datamodel/BaseModel')
+
+module.exports = {
+    findBaseByHeroID:function(hero_id){
+        return new Promise((resolved,reject) =>{
+            base.findOne({
+                hero_id:hero_id
+            },(err,document) => {
+                if(err){reject}
+                resolved(document)
+            })
+        })
+    },
+    findBaseByObjectID:function(objec_id){
+        return new Promise((resolved,reject) => {
+            base.findOne({
+                _id:objec_id
+            },(err,document) => {
+                if(err){reject}
+                resolved(document)
+            })
+        })
+    },
+
+    updateBaseHPByObjectID:function(hp,object_id){
+        return new Promise((resolved,reject) => {
+            let searchCondition = {"_id":object_id}
+            let updateCondition = {"base_hp":hp}
+            base.updateOne(searchCondition,updateCondition,(err,document) => {
+                if(err){reject}
+                resolved(document)
+            })
+        })
+    },
+
+    createRedBaseByHeroID: function (heroID) {
+        return new Promise((resolved, reject) => {
+            let newBase = new base({
+                tile_id: 52,
+                base_hp: 200,
+                hero_id: heroID,
+                camp: 'red'
+
+            })
+            newBase.save((err, document) => {
+                if (err) { reject }
+                resolved(document)
+            })
+        })
+    },
+    createBlueBaseByHeroID:function(heroID){
+        return new Promise((resolved,reject) => {
+            let newBase = new base({
+                tile_id: 192,
+                base_hp: 200,
+                hero_id: heroID,
+                camp: 'blue'
+            })
+
+            newBase.save((err,document) => {
+                if(err){reject}
+                resolved(document)
+            })
+        })
+    }
+}
