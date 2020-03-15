d351e55307c5f784f6a2788a371f497065778367
panghu <315782792@qq.com>
2020-03-15 20:39:32 +0800

    Client side
    Add RENDER_CONNECTION_BASE & RENDER_CONNECTION_HERO in EventType.js

--- client-side/assets/script/tools/EventType.js
+++ client-side/assets/script/tools/EventType.js
@@ -9,8 +9,11 @@ const SHOW_ATTACK_AREA = "SHOW_ATTACK_AREA"                 //bind with in hero-
 const HIDE_ATTACK_AREA = "HIDE_ATTACK_AREA"                 //bind with in hero-manager.js not use
 
 const REQUEST_FIGHT_OTHER = "REQUEST_FIGHT_OTHER"               //bind with in fight-manager.js
+const REQUEST_FIGHT_BASE = "REQUEST_FIGHT_BASE"                 //bind with in fight-manager.js
 
 const GET_ANIMATION_NAME = "GET_ANIMATION_NAME"             //bind with in animation-manager.js
+const RENDER_CONNECTION_HERO = "RENDER_CONNECTION_HERO"     //bind with in hero-manager.js
+const RENDER_CONNECTION_BASE = "RENDER_CONNECTION_BASE"     //bind with in building-manager.js
 
 
 module.exports = {
@@ -24,7 +27,10 @@ module.exports = {
     HIDE_ATTACK_AREA:HIDE_ATTACK_AREA,
 
     REQUEST_FIGHT_OTHER:REQUEST_FIGHT_OTHER,
+    REQUEST_FIGHT_BASE:REQUEST_FIGHT_BASE,
     
-    GET_ANIMATION_NAME:GET_ANIMATION_NAME
+    GET_ANIMATION_NAME:GET_ANIMATION_NAME,
+    RENDER_CONNECTION_HERO:RENDER_CONNECTION_HERO,
+    RENDER_CONNECTION_BASE:RENDER_CONNECTION_BASE
 
 }
