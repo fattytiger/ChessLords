ffc4b8e8e24b2a1415a79c163b2efad0a2b58b5c
panghu <315782792@qq.com>
2020-03-15 20:49:39 +0800

    Server side
    Format the code in map.js

--- server-site/chess-lord-server/mappart/map.js
+++ server-site/chess-lord-server/mappart/map.js
@@ -2,109 +2,126 @@ const COMMANDS = require('../commands')
 const clientManager = require('../handler/ClientManager')
 const heroOptions = require('../databaseOption/HeroOptions')
 const troopOptions = require('../databaseOption/TroopOptions')
+const baseOptions = require('../databaseOption/BaseOptions')
 
 const RED_CAMP = 'red'
 const BLUE_CAMP = 'blue'
 module.exports = {
 
-    mapdata:function(client,parameters){
+    mapdata: function (client, parameters) {
         //get hero id
         let heroID = parameters[0]
 
         //find the hero camp
         heroOptions.findHeroByHeroID(heroID)
-        .then((document) => {
-            let troops = document.troops
-            let heroCamp = document.camp
-            if(troops.length === 0){
-                this.originMapData(heroID,heroCamp)
-            }
-            if(troops.length !== 0){
-                this.masterMapData(heroID,heroCamp)
-            }
-        })
+            .then((document) => {
+                let troops = document.troops
+                let heroCamp = document.camp
+                if (troops.length === 0) {
+                    this.originMapData(heroID, heroCamp)
+                }
+                if (troops.length !== 0) {
+                    this.masterMapData(heroID, heroCamp)
+                }
+            })
     },
 
-    originMapData:function(hero_id){
+    originMapData: function (hero_id) {
         let heroID = hero_id
         //create the solider
+
+        //init your troops
         heroOptions.findHeroByHeroID(heroID)
-        .then(document => {
-            this.camp = document.camp
-            if(this.camp === RED_CAMP){
-                return troopOptions.createRedSoldierByHeroID(heroID)
-            }
-            if(this.camp === BLUE_CAMP){
-                return troopOptions.createBlueSoldierByHeroID(heroID)
-            }
-        }).then(document => {
-            if(document.camp === RED_CAMP){
-                return heroOptions.pushTroopByHeroID(document.hero_id,document._id)
-            }
-            if(document.camp === BLUE_CAMP){
-                return heroOptions.pushTroopByHeroID(document.hero_id,document._id)
-            }
-        }).then(res => {
-            if(this.camp === RED_CAMP){
-                return troopOptions.createRedCavalryByHeroID(heroID)
-            }
-            if(this.camp === BLUE_CAMP){
-                return troopOptions.createBlueCavalryByHeroID(heroID)
-            }
-        }).then(document => {
-            if(this.camp === RED_CAMP){
-                return heroOptions.pushTroopByHeroID(document.hero_id,document._id)
-            }
-            if(this.camp === BLUE_CAMP){
-                return heroOptions.pushTroopByHeroID(document.hero_id,document._id)
-            }
-        }).then(res => {
-            if(this.camp === RED_CAMP){
-                return troopOptions.createRedArcherByHeroID(heroID)
-            }
-            if(this.camp === BLUE_CAMP){
-                return troopOptions.createBlueArcherByHeroID(heroID)
-            }
-        }).then(document => {
-            if(this.camp === RED_CAMP){
-                return heroOptions.pushTroopByHeroID(document.hero_id,document._id)
-            }
-            if(this.camp === BLUE_CAMP){
-                return heroOptions.pushTroopByHeroID(document.hero_id,document._id)
-            }
-        }).then(res => {
-            return heroOptions.findHeroByHeroID(heroID)
-        }).then(document => {
-            this.sendTroops(heroID,document.troops)
-        })
+            .then(document => {
+                this.camp = document.camp
+                if (this.camp === RED_CAMP) {
+                    return troopOptions.createRedSoldierByHeroID(heroID)
+                }
+                if (this.camp === BLUE_CAMP) {
+                    return troopOptions.createBlueSoldierByHeroID(heroID)
+                }
+            }).then(document => {
+                if (document.camp === RED_CAMP) {
+                    return heroOptions.pushTroopByHeroID(document.hero_id, document._id)
+                }
+                if (document.camp === BLUE_CAMP) {
+                    return heroOptions.pushTroopByHeroID(document.hero_id, document._id)
+                }
+            }).then(res => {
+                if (this.camp === RED_CAMP) {
+                    return troopOptions.createRedCavalryByHeroID(heroID)
+                }
+                if (this.camp === BLUE_CAMP) {
+                    return troopOptions.createBlueCavalryByHeroID(heroID)
+                }
+            }).then(document => {
+                if (this.camp === RED_CAMP) {
+                    return heroOptions.pushTroopByHeroID(document.hero_id, document._id)
+                }
+                if (this.camp === BLUE_CAMP) {
+                    return heroOptions.pushTroopByHeroID(document.hero_id, document._id)
+                }
+            }).then(res => {
+                if (this.camp === RED_CAMP) {
+                    return troopOptions.createRedArcherByHeroID(heroID)
+                }
+                if (this.camp === BLUE_CAMP) {
+                    return troopOptions.createBlueArcherByHeroID(heroID)
+                }
+            }).then(document => {
+                return heroOptions.pushTroopByHeroID(document.hero_id, document._id)
+            })
+
+            //init the base info
+            .then(() => {
+                if (this.camp === RED_CAMP) {
+                    return baseOptions.createRedBaseByHeroID(heroID)
+                }
+                if (this.camp === BLUE_CAMP) {
+                    return baseOptions.createBlueBaseByHeroID(heroID)
+                }
+            }).then(document => {
+                console.log(document)
+                return heroOptions.updateBaseByHeroID(heroID,document._id)
+            }).then((res) => {
+                return heroOptions.findHeroByHeroID(heroID)
+            }).then(document => {
+                this.sendMapData(document.hero_id, document.troops, document.base)
+            })
     },
 
-    sendTroops:function(hero_id,troops){
+    sendMapData: function (hero_id, troops, base) {
         let heroID = hero_id
         let troopArr = []
         troops.forEach(troop => {
             troopOptions.findTroopByObjecyID(troop)
-            .then(document => {
-                //push the troops
-                troopArr.push(document)
-                if(troopArr.length === 3){
-                    console.log('send data to client')
-                    let hero = {}
-                    hero.hero_id = heroID
-                    hero.troops = troopArr
-                    clientManager.sendBroadCastData(COMMANDS.MAP_DATA,hero)
-                }
-            })
+                .then(document => {
+                    //push the troops
+                    troopArr.push(document)
+                    if (troopArr.length === 3) {
+
+                        baseOptions.findBaseByHeroID(heroID)
+                            .then(document => {
+                                let map = {}
+                                map.hero = {}
+                                map.base = document
+                                map.hero.hero_id = heroID
+                                map.hero.troops = troopArr
+                                //send map data
+                                clientManager.sendBroadCastData(COMMANDS.MAP_DATA, map)
+                            })                        
+                    }
+                })
         });
     },
 
 
-    masterMapData:function(hero_id,hero_camp){
+    masterMapData: function (hero_id, hero_camp) {
         let heroID = hero_id
         heroOptions.findHeroByHeroID(heroID)
-        .then(document => {
-            this.sendTroops(heroID,document.troops)
-        })
+            .then(document => {
+                this.sendMapData(heroID, document.troops)
+            })
     }
-    
+
 }
