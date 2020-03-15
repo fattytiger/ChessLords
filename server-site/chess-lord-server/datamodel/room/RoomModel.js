99860ea83437b78386eeac2e76e6dff872dd5b8a
panghu <315782792@qq.com>
2020-03-15 20:44:35 +0800

    Server side
    Delete the TroopModel.js

--- server-site/chess-lord-server/datamodel/room/RoomModel.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-const Schema   = mongoose.Schema
-const roomSchema = new Schema({
-    heros:{type:Array,require:true},
-    room_name:{type:String,require:true}
-})
-module.exports = mongoose.model('room',roomSchema)
